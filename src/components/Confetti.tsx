2239755ed27d9e4cbca27615b6ea4caa7a8ce6ae
Arnaav Sareen <arnaav24sareen@gmail.com>
2025-11-26 15:24:26 -0500

    Refactor Confetti component to remove explicit type annotation for interval variable

--- src/components/Confetti.tsx
+++ src/components/Confetti.tsx
@@ -17,7 +17,7 @@ export const Confetti: React.FC<ConfettiProps> = ({ trigger }) => {
             return Math.random() * (max - min) + min;
         }
 
-        const interval: NodeJS.Timeout = setInterval(function() {
+        const interval = setInterval(function() {
             const timeLeft = animationEnd - Date.now();
 
             if (timeLeft <= 0) {
